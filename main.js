563670b59ad3cd2c2d3b4de5573ca2d80074d6c5
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-03-08 14:45:41 -0600

    [Add all changes]

--- main.js
+++ main.js
@@ -1,43 +1,117 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num1, num2) {
+  return num1 % num2 === 0 ? true : false
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  return num **2/2
 }
 
-function isLong() {
+function isLong(str) {
+  return str.length >= 15 ? true : false;
 }
 
-function exclaim() {
+function exclaim(str) {
+  if(str[str.length -2] !== '!') {
+    return str.includes ('!') ? str : str + '!';
+  } else {
+    for (let i = 0; i < str.length -1; i ++) {
+      if (str[i].includes('!')) {
+        return str.slice(0, i + 1);
+      }
+    }
+  }
 }
 
-function countWords() {
+function countWords(str) {
+  let arr = str.split(' ');
+  return arr.length;
 }
 
-function containsDigit() {
+function containsDigit(str) {
+  const numbers = '54321';
+  for (let i = 0; i < str.length; i ++) {
+    if (numbers.includes(str[i])) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsLowerCase() {
+function containsLowerCase(str) {
+  const lowerCasePhrase = 'everyletterislowercase'
+  for(let i = 0; i<str.length; i++){
+    if (lowerCasePhrase.includes(str[i])){
+      return true;
+    }
+  } return false;
 }
 
-function containsUpperCase() {
+function containsUpperCase(str) {
+  const upperCaseAbcs = 'ABCDEFGHIGKLMNOPQRSTUVWXYZ'
+  for(let i = 0; i<str.length; i++){
+    if (upperCaseAbcs.includes(str[i])){
+      return true;
+    }
+  } return false;
 }
 
-function containsNonAlphanumeric() {
+function containsNonAlphanumeric(str) {
+  const nonNumsOrLetters = '!@#$%^&*()- '
+  for(let i = 0; i< str.length; i++){
+    if (nonNumsOrLetters.includes(str[i])){
+      return true;
+    }
+} return false;
 }
 
-function containsSpace() {
+function containsSpace(str) {
+  for (let i = 0; i < str.length; i ++) {
+    if (str[i] === ' ') {
+      return true;
+    }
+  }
+  return false;
 }
 
-function digits() {
+function digits(nums) {
+  let newArr = [];
+  let str = nums.toString()
+  let arr = str.split('')
+  for(let i = 0; i < arr.length; i ++){
+    if (isNaN(arr[i]) === false)
+    newArr.push(Number(arr[i]))
+  }
+  return newArr
 }
 
-function truncate() {
+function truncate(str) {
+  if (str.length >= 15) {
+    return str.slice(0, 8) + '...';
+  }
+  return str;
 }
 
-function isValidPassword() {
+function isValidPassword(str) {
+  if (containsNonAlphanumeric(str) === true
+  && containsUpperCase(str) === true 
+  && containsLowerCase(str) == true 
+  && containsSpace(str) === false
+  && containsDigit(str) === true ){
+    return true;
+  }
+  return false;
 }
 
-function onlyPunchy() {
+function onlyPunchy(arr) {
+  const newArr = [];
+  const finalArr = [];
+  for (let i = 0; i < arr.length; i ++) {
+    newArr.push(exclaim(arr[i]))
+    if(isLong(newArr[i]) === false) {
+      finalArr.push(newArr[i])
+    }
+  }
+  return finalArr;
 }
 
 
